ad1eeb536668b81cc985ed5187edffe3483df266
Eby <cybe1990@gmail.com>
2025-03-30 15:52:36 +0100

    created the shop_app

--- trendy_trolley/shop_app/apps.py
+++ trendy_trolley/shop_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ProductsConfig(AppConfig):
+class ShopAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'products'
+    name = 'shop_app'
--- trendy_trolley/trendy_trolley/settings.py
+++ trendy_trolley/trendy_trolley/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'products',
+    'shop_app'
+
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'core.CustomUser'
