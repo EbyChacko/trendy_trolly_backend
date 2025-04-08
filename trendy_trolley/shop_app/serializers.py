ff9ded9fe6552444c6a3df835044f44f6f0f145e
Eby <cybe1990@gmail.com>
2025-04-08 12:35:22 +0100

    add product details serializers

--- trendy_trolley/shop_app/serializers.py
+++ trendy_trolley/shop_app/serializers.py
@@ -5,3 +5,15 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+class DetailedProductSerializer(serializers.ModelSerializer):
+    similar_products = serializers.SerializerMethodField()
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'price', 'image', 'slug', 'similar_products']
+    
+    def get_similar_products(self, product):
+        products = Product.objects.filter(category=product.category).exclude(id=product.id)
+        serializer = ProductSerializer(products, many=True)
+        return serializer.data
